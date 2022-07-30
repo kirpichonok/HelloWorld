ae83b799499b35403d76918b6ab20d6f67ab5109
kirpichonok <ivankirpichonok@gmail.com>
2022-07-30 13:39:25 +0300

    added appearing and hidden of button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButtom: UIButton!
+    @IBOutlet var HelloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        HelloWorldLabel.isHidden = true
+        toggleButtom.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func makeButtomAction() {
+        if HelloWorldLabel.isHidden {
+            HelloWorldLabel.isHidden = false
+            toggleButtom.setTitle("Hide text", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden =  true
+            toggleButtom.setTitle("Show text", for: .normal)
+        }
+    }
+    
 
 }
 
